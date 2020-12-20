cbb2e800f6f87ad48ebdf5f494230b414acc8e8d
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2020-12-20 19:50:29 +0600

    connected to db

--- bot.js
+++ bot.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
 //secret token sent from discord gods
 const token=process.env.BOT_TOKEN;
+//secret tokens sent from mongodb gods UwU
+const dbUser=process.env.DB_USER;
+const dbName=process.env.DB_NAME;
+const dbPass=process.env.PASSWORD;
+const collectionName=process.env.COLLECTION;
 const Discord = require('discord.js');
 const client = new Discord.Client({
   partials: ['MESSAGE']
 });
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.hzpkp.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const clientMongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //prefix
 const prefix = '/';
 //commands
 const addmod= 'mod-meh';
 const say = 'say';
+const info= 'info';
 client.on('ready', () => {
     console.log('our bot is ready to go');
 });
@@ -29,9 +38,18 @@ client.on('message', message => {
     message.react('😍')
   }
 
-  if(message.content.includes(`${prefix}${say}`) && message.content.length > 4){
+  if(message.content.startsWith(`${prefix}${say}`) && message.content.length > 4){
     const speech= message.content.slice(4);
     message.channel.send(speech);
+  }
+  
+  if(message.content.startsWith(`${prefix}${info}`) && message.content.length > 5){
+    clientMongo.connect(err => {
+      const collection = clientMongo.db(dbName).collection(collectionName);
+      // perform actions on the collection object
+      console.log("connected");
+      clientMongo.close();
+    });
   }
 });
 
