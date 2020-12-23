b1c346f99693f3b90cc1020457e0a5612bbfb11d
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2020-12-23 21:08:36 +0600

    failed to use mongodb...gonna use firestore now

--- bot.js
+++ bot.js
@@ -6,12 +6,14 @@ const dbUser=process.env.DB_USER;
 const dbName=process.env.DB_NAME;
 const dbPass=process.env.PASSWORD;
 const collectionName=process.env.COLLECTION;
-
+const cors = require('cors');
+const bodyParser= require('body-parser');
 //the discord setup
 const Discord = require('discord.js');
 const client = new Discord.Client({
   partials: ['MESSAGE']
 });
+
 //mongodb setup
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.hzpkp.mongodb.net/${dbName}?retryWrites=true&w=majority`;
@@ -21,7 +23,9 @@ const prefix = '/';
 //commands
 const addmod= 'mod-meh';
 const say = 'say';
-const info= 'info';
+const name= 'name';
+const favLang = "lang";
+
 //letting us know if bot is ready
 client.on('ready', () => {
     console.log('our bot is ready to go');
@@ -30,6 +34,8 @@ client.on('ready', () => {
 client.on('messageDelete', msg => {
   msg.channel.send('stop deleting shit you pussy');
 })
+
+
 client.on('message', message => {
   //giving a mod role
   if (message.content === `${prefix}${addmod}`) {
@@ -45,15 +51,37 @@ client.on('message', message => {
     const speech= message.content.slice(4);
     message.channel.send(speech);
   }
-  //personal info command execution
-  if(message.content.startsWith(`${prefix}${info}`) && message.content.length > 5){
-    //sending data to database(coming up in future commits)
-    clientMongo.connect(err => {
-      const collection = clientMongo.db(dbName).collection(collectionName);
-      console.log("connected");
-      clientMongo.close();
-    });
-  }
+  clientMongo.connect(err => {
+    const collection = clientMongo.db(dbName).collection(collectionName);
+
+    //personal info command execution
+    if(message.content.startsWith(`${prefix}${name}`) && message.content.length > 5){
+      collection.insertOne({
+        "name": message.content.slice(6)
+      })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+    if(message.content.startsWith(`${prefix}${favLang}`) && message.content.length > 5){
+      collection.insertOne(
+        {
+          "favlang": message.content.slice(5)
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+    
+  });
+  
 });
 
 client.login(token);
