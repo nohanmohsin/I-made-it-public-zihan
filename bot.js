747341949256719fe954377b196b52b6f2e4dfde
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2020-12-25 14:52:50 +0600

    hehehehe sike I used mongoose -.-

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
 //secret token sent from discord gods
-const token=process.env.BOT_TOKEN;
+const token = process.env.BOT_TOKEN;
 //secret tokens sent from mongodb gods UwU
-const dbUser=process.env.DB_USER;
-const dbName=process.env.DB_NAME;
-const dbPass=process.env.PASSWORD;
-const collectionName=process.env.COLLECTION;
-const cors = require('cors');
-const bodyParser= require('body-parser');
+const dbUser = process.env.DB_USER;
+const dbName = process.env.DB_NAME;
+const dbPass = process.env.PASSWORD;
+const collectionName = process.env.COLLECTION;
 //the discord setup
 const Discord = require('discord.js');
 const client = new Discord.Client({
@@ -15,20 +13,23 @@ const client = new Discord.Client({
 });
 
 //mongodb setup
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.hzpkp.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-const clientMongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.4elyp.mongodb.net/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true })
+const personalSchema = new mongoose.Schema({
+  Name: String,
+  Lang: String
+})
+const personalModel = mongoose.model('datas', personalSchema);
 //prefix
 const prefix = '/';
 //commands
-const addmod= 'mod-meh';
+const addmod = 'mod-meh';
 const say = 'say';
-const name= 'name';
-const favLang = "lang";
+const info= 'info';
 
 //letting us know if bot is ready
 client.on('ready', () => {
-    console.log('our bot is ready to go');
+  console.log('our bot is ready to go');
 });
 //actions if a message if deleted
 client.on('messageDelete', msg => {
@@ -43,45 +44,24 @@ client.on('message', message => {
     message.member.roles.add(mod);
   }
   //reacting to specific message
-  if (message.content === 'ye bish stonks'){
+  if (message.content === 'ye bish stonks') {
     message.react('😍')
   }
   //saying something command execution
-  if(message.content.startsWith(`${prefix}${say}`) && message.content.length > 4){
-    const speech= message.content.slice(4);
+  if (message.content.startsWith(`${prefix}${say}`) && message.content.length > 4) {
+    const speech = message.content.slice(4);
     message.channel.send(speech);
   }
-  clientMongo.connect(err => {
-    const collection = clientMongo.db(dbName).collection(collectionName);
-
-    //personal info command execution
-    if(message.content.startsWith(`${prefix}${name}`) && message.content.length > 5){
-      collection.insertOne({
-        "name": message.content.slice(6)
-      })
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-    if(message.content.startsWith(`${prefix}${favLang}`) && message.content.length > 5){
-      collection.insertOne(
-        {
-          "favlang": message.content.slice(5)
-        }
-      )
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-    
-  });
-  
+  if(message.content.startsWith(`${prefix}${info}`) && message.content.length > 5){
+    const realName = message.content.slice(5);
+    let lang = message.content.split(' ')[2];
+    let info = new personalModel({
+      Name: realName,
+      Lang: lang
+    }) 
+    info.save()
+    .then(res => {console.log('saved name')})
+  }
 });
 
 client.login(token);
